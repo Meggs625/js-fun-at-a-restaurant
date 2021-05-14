61c6d3a31b488f8f65302f1657aeb6a0f59d390a
Megan McBride <megan.mcbride@colorado.edu>
2021-05-14 16:14:44 -0600

    Update with successful meal test

--- src/meal.js
+++ src/meal.js
@@ -1,15 +1,51 @@
-function nameMenuItem() {
-
+function nameMenuItem(foodChoice) {
+  return `Delicious ${foodChoice}`;
 }
 
+function createMenuItem(menuItemName, price, type) {
+  var menuItem = {
+    name: menuItemName,
+    price: price,
+    type: type,
+  }
+  return menuItem;
+}
+
+
+function addIngredients(firstIngredient, ingredients) {
+
+if ( ingredients.includes(firstIngredient,) === false) {
+  ingredients.push(firstIngredient,);
+}
+  return ingredients;
+};
+
+
+function formatPrice(initialPrice) {
+  return `$${initialPrice}`;
+};
+
+
+function decreasePrice(discountedPrice) {
+  return discountedPrice - (discountedPrice * 0.1);
+};
+
+
+function createRecipe(title, ingredients, type) {
+  var recipe = {
+    title: title,
+    ingredients: ingredients,
+    type: type,
+  }
+
+  return recipe;
+};
 
 module.exports = {
   nameMenuItem,
-  // createMenuItem,
-  // addIngredients,
-  // formatPrice,
-  // decreasePrice,
-  // createRecipe
+  createMenuItem,
+  addIngredients,
+  formatPrice,
+  decreasePrice,
+  createRecipe
 }
-
-
